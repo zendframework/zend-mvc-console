20718d0abe8611bed1c86d8540358a7c2ded9ea8
Matthew Weier O'Phinney <matthew@zend.com>
2016-03-22 09:06:45 -0500

    Added tests for ExceptionStrategy factory

---
+++ test/Service/ConsoleExceptionStrategyFactoryTest.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/zend-mvc-console for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Mvc\Console\Service;
+
+use Interop\Container\ContainerInterface;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Mvc\Console\Service\ConsoleExceptionStrategyFactory;
+use Zend\Mvc\Console\View\ExceptionStrategy;
+
+class ConsoleExceptionStrategyFactoryTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->container = $this->prophesize(ContainerInterface::class);
+        $this->factory = new ConsoleExceptionStrategyFactory();
+    }
+
+    public function testEnablesDisplayExceptionsWithoutConfiguration()
+    {
+        $this->container->has('config')->willReturn(false);
+
+        $strategy = $this->factory->__invoke($this->container->reveal(), ExceptionStrategy::class);
+        $this->assertInstanceOf(ExceptionStrategy::class, $strategy);
+        $this->assertTrue($strategy->displayExceptions());
+    }
+
+    public function testProvidesEmptyExceptionMessageWithoutConfiguration()
+    {
+        $this->container->has('config')->willReturn(false);
+
+        $strategy = $this->factory->__invoke($this->container->reveal(), ExceptionStrategy::class);
+        $this->assertInstanceOf(ExceptionStrategy::class, $strategy);
+        $this->assertEquals('', $strategy->getMessage());
+    }
+
+    public function overrideDisplayExceptionsConfiguration()
+    {
+        return [
+            'console' => [[
+                'console' => ['view_manager' => [
+                    'display_exceptions' => false,
+                ]]
+            ]],
+            'default' => [[
+                'view_manager' => [
+                    'display_exceptions' => false,
+                ]
+            ]],
+        ];
+    }
+
+    /**
+     * @dataProvider overrideDisplayExceptionsConfiguration
+     */
+    public function testCanOverrideDisplayExceptionsFlagViaConfiguration($config)
+    {
+        $this->container->has('config')->willReturn(true);
+        $this->container->get('config')->willReturn($config);
+
+        $strategy = $this->factory->__invoke($this->container->reveal(), ExceptionStrategy::class);
+        $this->assertInstanceOf(ExceptionStrategy::class, $strategy);
+        $this->assertFalse($strategy->displayExceptions());
+    }
+
+    public function overrideExceptionMessageConfiguration()
+    {
+        return [
+            'console' => [[
+                'console' => ['view_manager' => [
+                    'exception_message' => 'MESSAGE',
+                ]]
+            ], 'MESSAGE'],
+            'default' => [[
+                'view_manager' => [
+                    'exception_message' => 'MESSAGE',
+                ]
+            ], 'MESSAGE'],
+        ];
+    }
+
+    /**
+     * @dataProvider overrideExceptionMessageConfiguration
+     */
+    public function testCanOverrideExceptionMessageViaConfiguration($config, $expected)
+    {
+        $this->container->has('config')->willReturn(true);
+        $this->container->get('config')->willReturn($config);
+
+        $strategy = $this->factory->__invoke($this->container->reveal(), ExceptionStrategy::class);
+        $this->assertInstanceOf(ExceptionStrategy::class, $strategy);
+        $this->assertEquals($expected, $strategy->getMessage());
+    }
+}
