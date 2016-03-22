9a3d23253325162d8a6aaf87fd97fde7b7bd1e0d
Matthew Weier O'Phinney <matthew@zend.com>
2016-03-22 09:13:29 -0500

    Tested request/response delegator factories.

---
+++ test/Service/ConsoleRequestDelegatorFactoryTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/zend-mvc-console for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Mvc\Console\Service;
+
+use Interop\Container\ContainerInterface;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Console\Request;
+use Zend\Mvc\Console\Service\ConsoleRequestDelegatorFactory;
+
+class ConsoleRequestDelegatorFactoryTest extends TestCase
+{
+    use FactoryEnvironmentTrait;
+
+    public function setUp()
+    {
+        $this->factory = new ConsoleRequestDelegatorFactory();
+    }
+
+    public function testReturnsReturnValueOfCallbackWhenNotInConsoleEnvironment()
+    {
+        $this->setConsoleEnvironment(false);
+
+        $callback = function () {
+            return 'FOO';
+        };
+
+        $this->assertSame(
+            $callback(),
+            $this->factory->__invoke($this->createContainer(), 'Request', $callback)
+        );
+    }
+
+    public function testReturnsConsoleRequestWhenInConsoleEnvironment()
+    {
+        $this->setConsoleEnvironment(true);
+
+        $callback = function () {
+            return 'FOO';
+        };
+
+        $result = $this->factory->__invoke($this->createContainer(), 'Request', $callback);
+        $this->assertInstanceOf(Request::class, $result);
+    }
+}
---
+++ test/Service/ConsoleResponseDelegatorFactoryTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/zend-mvc-console for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Mvc\Console\Service;
+
+use Interop\Container\ContainerInterface;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Console\Response;
+use Zend\Mvc\Console\Service\ConsoleResponseDelegatorFactory;
+
+class ConsoleResponseDelegatorFactoryTest extends TestCase
+{
+    use FactoryEnvironmentTrait;
+
+    public function setUp()
+    {
+        $this->factory = new ConsoleResponseDelegatorFactory();
+    }
+
+    public function testReturnsReturnValueOfCallbackWhenNotInConsoleEnvironment()
+    {
+        $this->setConsoleEnvironment(false);
+
+        $callback = function () {
+            return 'FOO';
+        };
+
+        $this->assertSame(
+            $callback(),
+            $this->factory->__invoke($this->createContainer(), 'Response', $callback)
+        );
+    }
+
+    public function testReturnsConsoleResponseWhenInConsoleEnvironment()
+    {
+        $this->setConsoleEnvironment(true);
+
+        $callback = function () {
+            return 'FOO';
+        };
+
+        $result = $this->factory->__invoke($this->createContainer(), 'Response', $callback);
+        $this->assertInstanceOf(Response::class, $result);
+    }
+}
