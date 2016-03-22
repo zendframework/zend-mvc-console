50d9a38b93864753d183d12a1fb8bfc1bf36a98e
Matthew Weier O'Phinney <matthew@zend.com>
2016-03-22 09:22:58 -0500

    Tested ConsoleResponseSenderDelegatorFactory

---
+++ test/Service/ConsoleResponseSenderDelegatorFactoryTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/zend-mvc-console for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Mvc\Console\Service;
+
+use Interop\Container\ContainerInterface;
+use PHPUnit_Framework_TestCase as TestCase;
+use Prophecy\Argument;
+use Zend\EventManager\EventManagerInterface;
+use Zend\EventManager\EventsCapableInterface;
+use Zend\Mvc\Console\ResponseSender\ConsoleResponseSender;
+use Zend\Mvc\Console\Service\ConsoleResponseSenderDelegatorFactory;
+use Zend\Mvc\ResponseSender\SendResponseEvent;
+
+class ConsoleResponseSenderDelegatorFactoryTest extends TestCase
+{
+    public function testAttachesConsoleResponseSenderToSendResponseListener()
+    {
+        $events = $this->prophesize(EventManagerInterface::class);
+        $events->attach(
+            SendResponseEvent::EVENT_SEND_RESPONSE,
+            Argument::type(ConsoleResponseSender::class),
+            -2000
+        )->shouldBeCalled();
+
+        $listener = $this->prophesize(EventsCapableInterface::class);
+        $listener->getEventManager()->willReturn($events->reveal());
+
+        $container = $this->prophesize(ContainerInterface::class)->reveal();
+
+        $callback = function () use ($listener) {
+            return $listener->reveal();
+        };
+
+        $factory = new ConsoleResponseSenderDelegatorFactory();
+        $this->assertSame(
+            $listener->reveal(),
+            $factory($container, 'SendResponseListener', $callback)
+        );
+    }
+}
