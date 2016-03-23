1969efaead70816597ca0acb66734b270f7c87ea
Matthew Weier O'Phinney <matthew@zend.com>
2016-03-23 08:59:45 -0500

    Better test coverage of RouteNotFoundStrategy

---
+++ test/View/TestAsset/ConsoleModule.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/zend-mvc-console for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Mvc\Console\View\TestAsset;
+
+class ConsoleModule
+{
+    public function getConsoleBanner()
+    {
+    }
+
+    public function getConsoleUsage()
+    {
+        
+    }
+}
