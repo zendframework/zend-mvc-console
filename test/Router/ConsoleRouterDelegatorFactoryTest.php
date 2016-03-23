365c95a3a6235a26f15924ee464ad02ee47166f3
Matthew Weier O'Phinney <matthew@zend.com>
2016-03-23 09:59:28 -0500

    Added tests for ConsoleRouterDelegatorFactory

---
+++ test/Router/ConsoleRouterDelegatorFactoryTest.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/zend-mvc-console for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Mvc\Console\Router;
+
+use Interop\Container\ContainerInterface;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Mvc\Console\Router\ConsoleRouterDelegatorFactory;
+use ZendTest\Mvc\Console\Service\FactoryEnvironmentTrait;
+
+class ConsoleRouterDelegatorFactoryTest extends TestCase
+{
+    use FactoryEnvironmentTrait;
+
+    public function environments()
+    {
+        return [
+            'console' => [true],
+            'http'    => [false],
+        ];
+    }
+
+    /**
+     * @dataProvider environments
+     */
+    public function testReturnsOriginalServiceWhenRequestedServiceIsNotConsoleRouterOrRouter($consoleFlag)
+    {
+        $this->setConsoleEnvironment($consoleFlag);
+        $container = $this->prophesize(ContainerInterface::class);
+        $container->get('ConsoleRouter')->shouldNotBeCalled();
+        $factory   = new ConsoleRouterDelegatorFactory();
+
+        $this->assertEquals('FOO', $factory(
+            $container->reveal(),
+            'not-a-router',
+            function () {
+                return 'FOO';
+            }
+        ));
+    }
+
+    /**
+     * @dataProvider environments
+     */
+    public function testReturnsConsoleRouterServiceIfRequestedNameIsConsoleRouter($consoleFlag)
+    {
+        $this->setConsoleEnvironment($consoleFlag);
+        $container = $this->prophesize(ContainerInterface::class);
+        $container->get('ConsoleRouter')->willReturn('ConsoleRouter');
+
+        $factory   = new ConsoleRouterDelegatorFactory();
+
+        $this->assertEquals('ConsoleRouter', $factory(
+            $container->reveal(),
+            'ConsoleRouter',
+            function () {
+                return 'FOO';
+            }
+        ));
+    }
+
+    public function routerServiceNames()
+    {
+        return [
+            ['router'],
+            ['Router'],
+            ['ROUTER'],
+        ];
+    }
+
+    /**
+     * @dataProvider routerServiceNames
+     */
+    public function testReturnsConsoleRouterServiceIfRequestedNameIsRouterAndInConsoleEnvironment($routerServiceName)
+    {
+        $this->setConsoleEnvironment(true);
+        $container = $this->prophesize(ContainerInterface::class);
+        $container->get('ConsoleRouter')->willReturn('ConsoleRouter');
+
+        $factory   = new ConsoleRouterDelegatorFactory();
+
+        $this->assertEquals('ConsoleRouter', $factory(
+            $container->reveal(),
+            $routerServiceName,
+            function () {
+                return 'FOO';
+            }
+        ));
+    }
+
+    /**
+     * @dataProvider routerServiceNames
+     */
+    public function testReturnsOriginalServiceIfRequestedNameIsRouterAndNotInConsoleEnvironment($routerServiceName)
+    {
+        $this->setConsoleEnvironment(false);
+        $container = $this->prophesize(ContainerInterface::class);
+        $container->get('ConsoleRouter')->shouldNotBeCalled();
+
+        $factory   = new ConsoleRouterDelegatorFactory();
+
+        $this->assertEquals('FOO', $factory(
+            $container->reveal(),
+            $routerServiceName,
+            function () {
+                return 'FOO';
+            }
+        ));
+    }
+}
