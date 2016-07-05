835f4e1fddc32188d0dfacf7c11f0b19bf051cea
Matthias Tylkowski <matthias@binarypark.org>
2016-07-05 14:01:15 +0200

    coding standard

--- test/ServiceManagerTest.php
+++ test/ServiceManagerTest.php
@@ -15,48 +15,49 @@ use Zend\Mvc\Console\Service\ConsoleResponseSenderDelegatorFactory;
 
 class ServiceManagerTest extends TestCase
 {
-		public function testEventManagerOverridden()
-		{
-				$minimalConfig = [
-						'aliases' => [
-								'SendResponseListener' => SendResponseListener::class,
-						],
-						'factories' => [
-								SendResponseListener::class => InvokableFactory::class
-						],
-						'delegators' => [
-								SendResponseListener::class => [
-										function(ContainerInterface $container, $name, callable $callback, array $options = null) {
-												$consoleResponseSenderDelegatorFactory = new ConsoleResponseSenderDelegatorFactory();
-												$sendResponseListener = $consoleResponseSenderDelegatorFactory->__invoke($container, $name, $callback, $options);
-												$this->assertInstanceOf(SendResponseListener::class, $sendResponseListener);
+    public function testEventManagerOverridden()
+    {
+        $minimalConfig = [
+            'aliases' => [
+                'SendResponseListener' => SendResponseListener::class,
+            ],
+            'factories' => [
+                SendResponseListener::class => InvokableFactory::class
+            ],
+            'delegators' => [
+                SendResponseListener::class => [
+                    function(ContainerInterface $container, $name, callable $callback, array $options = null)
+                    {
+                        $consoleResponseSenderDelegatorFactory = new ConsoleResponseSenderDelegatorFactory();
+                        $sendResponseListener = $consoleResponseSenderDelegatorFactory->__invoke($container, $name, $callback, $options);
+                        $this->assertInstanceOf(SendResponseListener::class, $sendResponseListener);
 
-												$eventManager = $sendResponseListener->getEventManager();
-												$this->assertEvents($eventManager);
+                        $eventManager = $sendResponseListener->getEventManager();
+                        $this->assertEvents($eventManager);
 
-												return $sendResponseListener;
-										}
-								],
-						]
-				];
+                        return $sendResponseListener;
+                    }
+                ],
+            ]
+        ];
 
-				$smConfig = new ServiceManagerConfig($minimalConfig);
+        $smConfig = new ServiceManagerConfig($minimalConfig);
 
-				$serviceManager = new ServiceManager();
-				$smConfig->configureServiceManager($serviceManager);
+        $serviceManager = new ServiceManager();
+        $smConfig->configureServiceManager($serviceManager);
 
-				$sendResponseListener = $serviceManager->get('SendResponseListener');
-				$eventManager = $sendResponseListener->getEventManager();
-				$this->assertEvents($eventManager);
-		}
+        $sendResponseListener = $serviceManager->get('SendResponseListener');
+        $eventManager = $sendResponseListener->getEventManager();
+        $this->assertEvents($eventManager);
+    }
 
-		protected function assertEvents($eventManager)
-		{
-				$r = new ReflectionProperty($eventManager, 'events');
-				$r->setAccessible(true);
-				$events = $r->getValue($eventManager);
+    protected function assertEvents($eventManager)
+    {
+        $r = new ReflectionProperty($eventManager, 'events');
+        $r->setAccessible(true);
+        $events = $r->getValue($eventManager);
 
-				$this->assertEquals(4, count($events['sendResponse']));
-				$this->assertEquals(ConsoleResponseSender::class, get_class($events['sendResponse']['-2000.0'][0]));
-		}
+        $this->assertEquals(4, count($events['sendResponse']));
+        $this->assertEquals(ConsoleResponseSender::class, get_class($events['sendResponse']['-2000.0'][0]));
+    }
 }
